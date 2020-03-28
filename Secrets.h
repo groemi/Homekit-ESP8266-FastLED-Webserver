469313ed805a2b35c8e5170deddab5cf1e56971f
groemi <michaelgroetzner@gmx.net>
2020-03-28 15:48:24 +0100

    First Upload

---
+++ Secrets.h
@@ -0,0 +1,6 @@
+
+const char WiFiAPPSK[] = "64810FB6FC01218";
+// FRITZ!Box 4020 QP
+// Wi-Fi network to connect to (if not in AP mode)
+char* ssid = "FRITZ!Box 7430 RV";
+char* password = "64810FB6FC01218";
